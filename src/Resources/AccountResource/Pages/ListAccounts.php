1fd6cf62be741cfe456b73b17616c91ce064bae9
ibad <1040745+dulabs@users.noreply.github.com>
2023-07-08 21:04:07 +0000

    add account model

---
+++ src/Resources/AccountResource/Pages/ListAccounts.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Lakuko\FilamentSanctum\Resources\AccountResource\Pages;
+
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListAccounts extends ListRecords
+{
+    public static function getResource(): string
+    {
+        return Config::get('filament-sanctum.resources.AccountResource');
+    }
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
