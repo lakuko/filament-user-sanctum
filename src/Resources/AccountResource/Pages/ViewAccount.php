ce7bfbc84074c58e75cfe3d86d4c0b355f0b0462
ibad <1040745+dulabs@users.noreply.github.com>
2023-07-09 19:06:23 +0000

    remove reset action

--- src/Resources/AccountResource/Pages/ViewAccount.php
+++ src/Resources/AccountResource/Pages/ViewAccount.php
@@ -4,19 +4,11 @@ namespace Lakuko\FilamentSanctum\Resources\AccountResource\Pages;
 
 use Filament\Resources\Pages\ViewRecord;
 use Illuminate\Support\Facades\Config;
-use Lakuko\FilamentSanctum\Actions;
 
 class ViewAccount extends ViewRecord
 {
     public static function getResource(): string
     {
         return Config::get('filament-sanctum.resources.AccountResource');
-    }
-
-    protected function getActions(): array
-    {
-        return array_merge([
-            Actions\ResetAction::make(),
-        ], parent::getActions());
     }
 }
